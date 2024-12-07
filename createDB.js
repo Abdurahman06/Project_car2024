04a6e85efe361e1c0e7cea5e15793ded1452fd5a
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2024-12-07 21:33:34 +0300

    car_6_2

--- createDB.js
+++ createDB.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
+var data = require("./data.js").data;
+
+
 // Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -22,3 +25,10 @@ main()
 .then(console.log)
 .catch(console.error)
 .finally(() => client.close());
+
+
+
+
+
+
+
