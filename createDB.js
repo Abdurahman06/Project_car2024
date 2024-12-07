a661cb33fe495a0d5acbc657e010c4a69f3154f7
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2024-12-08 00:15:51 +0300

    car_6_3

--- createDB.js
+++ createDB.js
@@ -8,13 +8,13 @@ var data = require("./data.js").data;
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 // Database Name
-const dbName = 'test2024';
+const dbName = 'car2024';
 async function main() {
 // Use connect method to connect to the server
 await client.connect();
 console.log('Connected successfully to server');
 const db = client.db(dbName);
-const collection = db.collection('documents');
+const collection = db.collection('cars');
 // the following code examples can be pasted here...
 const insertResult = await collection.insertMany([{ a: 1 }, { a:
 2 }, { a: 3 }]);
