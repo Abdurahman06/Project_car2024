50aee991fb6b009080216a54ea186ad86b28100f
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2024-12-08 20:39:22 +0300

    car_8_4

--- routes/cars.js
+++ routes/cars.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Car = require('../models/car').Car;
 
-/* GET users listing. */
+/* GET cars listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars');
 });
