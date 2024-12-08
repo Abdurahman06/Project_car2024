ffcc53f8bad21968dcdc6ccd4b043d63975fceb3
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2024-12-08 19:53:59 +0300

    car_8_3

--- createDB.js
+++ createDB.js
@@ -7,20 +7,23 @@ var data = require("./data.js").data;
 // Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
+
 // Database Name
 const dbName = 'car2024';
+
 async function main() {
 // Use connect method to connect to the server
 await client.connect();
 console.log('Connected successfully to server');
 const db = client.db(dbName);
 const collection = db.collection('cars');
+
 // the following code examples can be pasted here...
-const insertResult = await collection.insertMany([{ a: 1 }, { a:
-2 }, { a: 3 }]);
+const insertResult = await collection.insertMany(data);
 console.log('Inserted documents =>', insertResult);
 return 'done.';
 }
+
 main()
 .then(console.log)
 .catch(console.error)
--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Car = require('../models/car').Car;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница машин */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var cars = await Car.find({nick: req.params.nick});
+    console.log(cars)
+    if(!cars.length) return next(new Error(" Нет такого автомобиля! "))
+    var car = cars[0];
+    res.render('car', {
+    title: car.title,
+    picture: car.avatar,
+    desc: car.desc
+    })
     });
+
 module.exports = router;
