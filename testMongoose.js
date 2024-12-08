25ba7c7a1b5a38e78aee5afd58b2761818e3050c
Abdurahman06 <habibullaevabdurrahman@gmail.com>
2024-12-08 11:55:02 +0300

    car_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.meow = function(){
-console.log(this.name + " сказал мяу")
-}
-const Cat = mongoose.model('Cat', schema);
-const kitty = new Cat({ name: 'Пушок' });
-kitty.save().then(() => kitty.meow());
+var Car = require('./models/car.js').Car
+var car = new Car({
+title: "Мерседес",
+nick: "mercedes",
+})
+car.save();
